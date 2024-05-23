3c3132895888b55a5dc6ed7425907c81f0f2abd5
HerbertKayky <herbertkayky9@gmail.com>
2024-05-23 18:35:31 -0300

    listando jogos e criando componente GameCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { GameCard } from "@/components/GameCard";
 import { Container } from "@/components/container";
 import { Input } from "@/components/input";
 import { GameProps } from "@/utils/types/game";
@@ -17,8 +18,20 @@ async function getDalyGame() {
   }
 }
 
+async function getGamesData() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, {
+      next: { revalidate: 320 },
+    });
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
 export default async function Home() {
   const dalyGame: GameProps = await getDalyGame();
+  const data: GameProps[] = await getGamesData();
 
   return (
     <main className="w-full ">
@@ -45,7 +58,13 @@ export default async function Home() {
             </div>
           </section>
         </Link>
-        <Input  />
+        <Input />
+        <h2 className="font-bold text-lg mt-8 mb-5">Jogos para conhecer</h2>
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map((item) => (
+            <GameCard key={item.id} data={item} />
+          ))}
+        </section>
       </Container>
     </main>
   );
